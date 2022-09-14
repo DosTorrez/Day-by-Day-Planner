13537f47faabb289f933c2af48be51e3e30c6fc3
Dosiano Torrez <103604527+DosTorrez@users.noreply.github.com>
2022-09-14 03:54:36 -0600

    Add files via upload

---
+++ Develop/script.js
@@ -0,0 +1,17 @@
+myStorage = window.localStorage; 
+// Variables for each hour 
+var hour8am = $("#8:00am");
+var hour9am = $("#9:00am");
+var hour10am = $("#10:00am");
+var hour11am = $("#11:00am");
+var hour12pm = $("#12:00pm");
+var hour1pm = $("#1:00pm");
+var hour2pm = $("#2:00pm");
+var hour3pm = $("#3:00pm");
+var hour4pm = $("#4:00pm");
+var hour5pm = $("#5:00pm");
+var hour6pm = $("#6:00pm");
+var hour7pm = $("#7:00pm");
+var hour8pm = $("#8:00pm");
+var hour9pm = $("#9:00pm");
+var hour10pm = $("#10:00pm");
